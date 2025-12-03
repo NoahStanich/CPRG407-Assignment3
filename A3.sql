4bbd776f8530b8136619ad48a597a74ce3f7c760
Penncilk <Jude.Uyeno@edu.sait.ca>
2025-12-03 00:43:41 -0700

    Fixed, even more bugs that were in the original, sorry Noah

--- A3.sql
+++ A3.sql
@@ -5,9 +5,10 @@ set feedback on;
 start "C:\cprg307\Assignment 3 Part 1 Scripts\create_wkis.sql";
 start "C:\cprg307\Assignment 3 Part 1 Scripts\constraints_wkis.sql";
 start "C:\cprg307\Assignment 3 Part 1 Scripts\load_wkis.sql";
+start "C:\cprg307\Assignment 3 Part 1 Scripts\A3_test_dataset_1_-_Clean[1].sql";
 
 DECLARE
-    CURSOR new_transactions_cursor (v_transaction_no new_transactions.transaction_no%TYPE) IS
+    CURSOR new_transactions_cursor IS
     SELECT account_no, transaction_no, transaction_type, transaction_amount, transaction_date, description
     FROM new_transactions;
 
@@ -17,19 +18,29 @@ DECLARE
     v_transaction_amount new_transactions.transaction_amount%TYPE;
     v_transaction_date new_transactions.transaction_date%TYPE;
     v_description new_transactions.description%TYPE;
+    v_old_transaction_no new_transactions.transaction_no%TYPE;
 BEGIN
-    OPEN new_transactions_cursor(v_transaction_no);
+    OPEN new_transactions_cursor;
+    DBMS_OUTPUT.PUT_LINE('test');
+    v_old_transaction_no := -1;
     LOOP
         FETCH new_transactions_cursor into v_account_no, v_transaction_no, v_transaction_type, v_transaction_amount, v_transaction_date, v_description;
         EXIT WHEN new_transactions_cursor%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE('test2');
+        DBMS_OUTPUT.PUT_LINE(v_transaction_no);
+        IF (v_old_transaction_no != v_transaction_no) THEN
+            INSERT INTO transaction_history(transaction_no, transaction_date, description)
+            VALUES(v_transaction_no, v_transaction_date, v_description);
+        END IF;
         INSERT INTO transaction_detail(account_no, transaction_no, transaction_type, transaction_amount)
         VALUES (v_account_no, v_transaction_no, v_transaction_type, v_transaction_amount);
-        INSERT INTO transaction_history(transaction_no, transaction_date, description)
-        VALUES(v_transaction_no, v_transaction_date, v_description);
+        
         -- Updates the accounts
         UPDATE ACCOUNT
         SET account_balance = v_transaction_amount
         WHERE account_no = v_account_no;
+        v_old_transaction_no := v_transaction_no;
+        
     END LOOP;
     CLOSE new_transactions_cursor;
 END;
